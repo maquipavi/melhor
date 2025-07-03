9d0667187eae9a36902d7775a7d3fa7c25303d12
maquipavi <maquipavi@gmail.com>
2025-07-03 15:41:51 -0300

    Update app_0.py

--- app_0.py
+++ app_0.py
@@ -20,7 +20,7 @@ warnings.filterwarnings("ignore")
 st.title("Sistema de Análise de Personalidade e Propósito de Vida!")
 
 # Configurar a API Key (fixo)
-api_key = "AIzaSyC0A8dnI6VfxyPux6ffDMoBpVCVpjDhKRs"
+api_key = "AIzaSyBb4NAieSTmH1u9GMnwLX9kXBOghqHghY0"
 os.environ["GOOGLE_API_KEY"] = api_key
 
 # Importações necessárias (só depois de configurar a API)
